76977cbeee1eea145b4b3c6b2eb350dcf046fafa
Daneil-k <158211667+Daneil-k@users.noreply.github.com>
2024-03-10 23:12:05 +0530

    Update send_file.py

--- send_file.py
+++ send_file.py
@@ -43,7 +43,8 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
                                                         InlineKeyboardButton('sᴛʀᴇᴀᴍ ᴏɴʟɪɴᴇ', url=lazy_stream)]])  # web stream Link
                 )
                 return await bot.copy_message(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
-                                          message_id=file_id, 
+                                          message_id=file_id,
+                                          protect_content="True"
                                           reply_markup=InlineKeyboardMarkup(
                                             [
                                                 [
@@ -67,6 +68,7 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
             )
             return await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
                                               message_ids=file_id,
+                                              protect_content="True"
                                               reply_markup=InlineKeyboardMarkup(
                                             [
                                                 [
